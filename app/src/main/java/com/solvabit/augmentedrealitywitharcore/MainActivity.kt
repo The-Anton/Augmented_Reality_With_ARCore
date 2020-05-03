03f969fa4b6812bbfc2fbf6f28898d0f2227337b
Shekhar <shekhar59324@gmail.com>
2020-05-03 22:53:40 +0530

    Setting Scene view for Display

--- app/src/main/java/com/solvabit/augmentedrealitywitharcore/MainActivity.kt
+++ app/src/main/java/com/solvabit/augmentedrealitywitharcore/MainActivity.kt
@@ -2,11 +2,26 @@ package com.solvabit.augmentedrealitywitharcore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.ar.sceneform.ux.ArFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var arFragment: ArFragment
+
+    private var isTracking: Boolean = false
+    private var isHitting: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        arFragment = sceneform_fragment as ArFragment
+
+        arFragment.arSceneView.scene.addOnUpdateListener { frameTime ->
+            arFragment.onUpdate(frameTime)
+            onUpdate()
+        }
+        floatingActionButton.setOnClickListener { addObject(Uri.parse("NOVELO_EARTH.sfb")) }
+        showFab(false)
+
     }
 }
